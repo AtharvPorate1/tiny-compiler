a66731f1eeca645466139c54eaae58d8453836da
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-04-18 00:02:03 +0530

    created parser file

--- compiler.js
+++ compiler.js
@@ -1,10 +1,16 @@
 const tokenizer = require('./tokenizer');
+const parser = require('./parser');
 
 module.exports = function compiler(input) {
     // 1. Lexical Analysis 
     // It will break the input into an array of tokens
     const tokens = tokenizer(input);
     // 2. Syntactic Analysis
+
+    // It will parse the array of tokens into an Abstract Syntax Tree (AST)
+    const ast = parser(tokens);
+
+
     // 3. Transformation
     // 4. Code Generation
     //
