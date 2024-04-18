a88327ca67aa41bd3d22717922989d78d6db37eb
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-04-18 08:20:16 +0530

    done with transformer

--- traverse.js
+++ traverse.js
@@ -1,4 +1,17 @@
-module.exports = function traverse(ast, visitors){
-    
-
-}
+module.exports = function traverse(ast, visitors) {
+    function walkNode(node, parent) {
+      const method = visitors[node.type];
+      if (method) {
+        method(node, parent);
+      }
+      if (node.type === 'Program') {
+        walkNodes(node.body, node);
+      } else if (node.type === 'CallExpression') {
+        walkNodes(node.params, node);
+      }
+    }
+    function walkNodes(nodes, parent) {
+      nodes.forEach(node => walkNode(node, parent));
+    }
+    walkNode(ast, null);
+  }
